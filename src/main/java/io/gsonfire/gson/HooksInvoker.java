5038e61300b0fd8338951955ed0aaa474087aa52
Andrew Siu <andrew.siu@collectivehealth.com>
2017-03-02 12:50:59 -0800

    using logger as well as stdout

--- src/main/java/io/gsonfire/gson/HooksInvoker.java
+++ src/main/java/io/gsonfire/gson/HooksInvoker.java
@@ -1,5 +1,12 @@
 package io.gsonfire.gson;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.log4j.Logger;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import io.gsonfire.annotations.PostDeserialize;
@@ -7,51 +14,42 @@ import io.gsonfire.annotations.PreSerialize;
 import io.gsonfire.util.reflection.AbstractMethodInspector;
 import io.gsonfire.util.reflection.MethodInvoker;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @autor: julio
  */
 public final class HooksInvoker {
 
     private static final Set<Class> SUPPORTED_TYPES = new HashSet<Class>(Arrays.asList(
-        JsonElement.class,
-        Gson.class
-    ));
+            JsonElement.class,
+            Gson.class));
 
     private AbstractMethodInspector<MethodInvoker> inspector = new AbstractMethodInspector<MethodInvoker>() {
         @Override
         protected MethodInvoker map(Method member) {
-            return new MethodInvoker(member, SUPPORTED_TYPES) ;
+            return new MethodInvoker(member, SUPPORTED_TYPES);
         }
     };
 
-    public HooksInvoker(){
+    public HooksInvoker() {
 
     }
 
-    public void preSerialize(Object obj){
+    public void preSerialize(Object obj) {
         invokeAll(obj, PreSerialize.class, null, null);
     }
 
-    public void postDeserialize(Object obj, JsonElement jsonElement, Gson gson){
+    public void postDeserialize(Object obj, JsonElement jsonElement, Gson gson) {
         invokeAll(obj, PostDeserialize.class, jsonElement, gson);
     }
 
-    private void invokeAll(Object obj, Class<? extends Annotation> annotation, JsonElement jsonElement, Gson gson){
-        if(obj != null) {
+    private void invokeAll(Object obj, Class<? extends Annotation> annotation, JsonElement jsonElement, Gson gson) {
+        if (obj != null) {
             for (MethodInvoker m : inspector.getAnnotatedMembers(obj.getClass(), annotation)) {
                 try {
                     m.invoke(obj, new HooksInvokerValueSupplier(jsonElement, gson));
-                } catch (IllegalAccessException e) {
+                } catch (IllegalAccessException | InvocationTargetException e) {
                     e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
+                    Logger.getLogger(this.getClass()).error("Error during object serialization", e);
                 }
             }
         }
@@ -69,7 +67,7 @@ public final class HooksInvoker {
 
         @Override
         public Object getValueForType(Class type) {
-            if(type == JsonElement.class) {
+            if (type == JsonElement.class) {
                 return jsonElement;
             } else if (type == Gson.class) {
                 return gson;
--- src/main/java/io/gsonfire/postprocessors/methodinvoker/MethodInvokerPostProcessor.java
+++ src/main/java/io/gsonfire/postprocessors/methodinvoker/MethodInvokerPostProcessor.java
@@ -1,5 +1,7 @@
 package io.gsonfire.postprocessors.methodinvoker;
 
+import java.lang.reflect.InvocationTargetException;
+import org.apache.log4j.Logger;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -7,8 +9,6 @@ import io.gsonfire.PostProcessor;
 import io.gsonfire.annotations.ExposeMethodResult;
 import io.gsonfire.gson.FireExclusionStrategy;
 import io.gsonfire.gson.FireExclusionStrategyComposite;
-
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * @autor: julio
@@ -29,24 +29,25 @@ public final class MethodInvokerPostProcessor<T> implements PostProcessor<T> {
 
     @Override
     public void postDeserialize(T result, JsonElement src, Gson gson) {
-        //nothing here
+        // nothing here
     }
 
     @Override
     public void postSerialize(JsonElement result, T src, Gson gson) {
-        if(result.isJsonObject()){
+        if (result.isJsonObject()) {
             JsonObject jsonObject = result.getAsJsonObject();
-            for(MappedMethod m: methodInspector.getAnnotatedMembers(src.getClass(), ExposeMethodResult.class)){
-                if(!serializationExclusionStrategy.shouldSkipMethod(m)) {
+            for (MappedMethod m : methodInspector.getAnnotatedMembers(src.getClass(), ExposeMethodResult.class)) {
+                if (!serializationExclusionStrategy.shouldSkipMethod(m)) {
                     try {
-                        if (m.getConflictResolutionStrategy() == ExposeMethodResult.ConflictResolutionStrategy.OVERWRITE || (m.getConflictResolutionStrategy() == ExposeMethodResult.ConflictResolutionStrategy.SKIP && !jsonObject.has(m.getSerializedName()))) {
+                        if (m.getConflictResolutionStrategy() == ExposeMethodResult.ConflictResolutionStrategy.OVERWRITE
+                                || (m.getConflictResolutionStrategy() == ExposeMethodResult.ConflictResolutionStrategy.SKIP
+                                        && !jsonObject.has(m.getSerializedName()))) {
                             Object value = m.getMethod().invoke(src);
                             jsonObject.add(m.getSerializedName(), gson.toJsonTree(value));
                         }
-                    } catch (IllegalAccessException e) {
+                    } catch (IllegalAccessException | InvocationTargetException e) {
                         e.printStackTrace();
-                    } catch (InvocationTargetException e) {
-                        e.printStackTrace();
+                        Logger.getLogger(this.getClass()).error("Error during object serialization", e);
                     }
                 }
             }
